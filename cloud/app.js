bb5b13fd9480bebd456d0e72714550eb6e472029
Chris Williams <cwilliams@alum.mit.edu>
2016-04-13 18:01:08 -0400

    changed PORT

--- cloud/app.js
+++ cloud/app.js
@@ -291,4 +291,4 @@ function getObject(className, userID, completionBlock) {
 
 console.log("********************Listening on port " + process.env.PORT);
 
-app.listen(process.env.PORT || 8080);
+//app.listen(process.env.PORT || 8080);
