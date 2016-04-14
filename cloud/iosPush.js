afd3d75e164185eaec9ff1994ae69b4e09564daf
Chris Williams <cwilliams@alum.mit.edu>
2016-04-14 12:58:22 -0400

    Server-side push notifications

---
+++ cloud/iosPush.js
@@ -0,0 +1,23 @@
+Parse.Cloud.define("iosPush", function(request, response) {
+
+  // request has 2 parameters: params passed by the client and the authorized user                                                                                                                               
+  var params = request.params;
+  var user = request.user;
+  var pushQuery = new Parse.Query(Parse.Installation);
+  pushQuery.equalTo('deviceType', 'ios'); // targeting iOS devices only                                                                                                                                          
+  pushQuery.equalTo('user', params.toUser)
+  params.delete('toUser');
+  
+  console.log("**************** Push: " + JSON.stringify(params);
+  
+  Parse.Push.send({
+    where: pushQuery, // Set our Installation query                                                                                                                                                              
+    data: data
+  }, { success: function() {
+      console.log("#### PUSH OK");
+  }, error: function(error) {
+      console.log("#### PUSH ERROR" + error.message);
+  }, useMasterKey: true});
+
+  response.success('success');
+});
