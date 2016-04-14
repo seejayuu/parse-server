40070265404d41d650a9e9fbe2a18e2a415b1b91
Chris Williams <cwilliams@alum.mit.edu>
2016-04-14 10:18:02 -0400

    Mailgun fixed for node.js

--- cloud/sendEmailToUser.js
+++ cloud/sendEmailToUser.js
@@ -1,4 +1,4 @@
-var client = require('./Mailgun.js')(apiKey: 'key-f7f17e392715c4328b9274a4557d08a5', domain: 'sandbox4ba3cd71927a419db74f6a84e97973f6.mailgun.org');
+var client = require('./Mailgun.js')({apiKey: 'key-f7f17e392715c4328b9274a4557d08a5', domain: 'sandbox4ba3cd71927a419db74f6a84e97973f6.mailgun.org'});
 
 Parse.Cloud.define("sendEmailToUser", function(request, response) {
   client.messages().send({
