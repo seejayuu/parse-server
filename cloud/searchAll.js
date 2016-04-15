9a59772744e7252b7285c43d4800822723dfa339
Chris Williams <cwilliams@alum.mit.edu>
2016-04-15 11:29:31 -0400

    Search in back end

--- cloud/searchAll.js
+++ cloud/searchAll.js
@@ -64,7 +64,7 @@ Parse.Cloud.define("searchAll", function(request, response) {
 		try {
 			_.each(results, function accum(r) { finalResults = finalResults.concat(r) });
 			console.log(JSON.stringify(finalResults[0]));
-			finalResults = _.sortBy(_.uniq(finalResults, function (a) { console.log(JSON.stringify(a.objectId)); return a["objectId"] }), function(a) { return Date.parse(a["createdAt"]) }).reverse();
+			finalResults = _.sortBy(_.uniq(finalResults, function (a) { console.log(typeof(a)); return a["objectId"] }), function(a) { return Date.parse(a["createdAt"]) }).reverse();
 		}
 		catch (e) {
 			console.log(e);
