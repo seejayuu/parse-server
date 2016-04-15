e3597d046cdb573fce534bce4abdd15c1d20e35f
Chris Williams <cwilliams@alum.mit.edu>
2016-04-15 15:29:37 -0400

    getPosts fix

--- cloud/readPosts.js
+++ cloud/readPosts.js
@@ -27,7 +27,6 @@ Parse.Cloud.define("readPosts", function(request, response) {
 		return query.find().then(function(results) {
 			try {
 				var users = _.filter(results, function(a) { return typeof(a) != "undefined" }).map(function(a) { return a.get("to")})
-				console.log("******************** queryPost2 users=" + users.length);
 				var query = getQuery("Post");
 				query.equalTo("type", "post").containedIn("createdBy", users).include("createdBy");
 				return query.find();
@@ -44,7 +43,7 @@ Parse.Cloud.define("readPosts", function(request, response) {
 		return userQuery.find().then(function(results) {
 			try {
 				console.log("******************** queryPost2a results=" + results.length);
-				var postingUsers = _.filter(postingUsers, function(a) { console.log(typeof(a.get("to"))); typeof(a.get("to")) != "undefined"} ).map(function(b) { return b.get("to")});
+				var postingUsers = _.filter(results, function(a) { console.log(typeof(a.get("to"))); typeof(a.get("to")) != "undefined"} ).map(function(b) { return b.get("to")});
 				console.log("******************** queryPost2a users=" + postingUsers.length);
 				var followQuery = getQuery("Follow");
 				followQuery.notEqualTo("type", "user").containedIn("from", postingUsers)
