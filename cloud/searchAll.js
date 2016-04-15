f3da5f27e0894900ec8d86a42baa4a558f5cf585
Chris Williams <cwilliams@alum.mit.edu>
2016-04-15 08:31:52 -0400

    Search in back end

---
+++ cloud/searchAll.js
@@ -0,0 +1,71 @@
+var _ = require("underscore");
+
+Parse.Cloud.define("searchAll", function(request, response) {
+	var Scope = {
+		All: 0,
+		Users: 1,
+		Posts: 2,
+		Albums: 3,
+		Groups: 4
+	};
+	var filter = request.params.filter;
+	var probe = request.params.probe;
+	var user = request.user;
+	var searchResults = [];
+	var promises = [];
+	function getQuery(className) {
+		return new Parse.Query(Parse.Object.extend(className));
+	}
+	if (filter == Scope.All || filter == Scope.Posts) {
+		// search posts and photos from roll
+		function queryPostField(field) {
+			var query = getQuery("Post");
+			query.equalTo("createdBy", user).matches("title", "(?i)" + probe).matches("title", "(?i)" + probe);
+			return query.find();
+		}
+		var tagQuery = getQuery("Tag");
+		tagQuery.matches("name", "(?i)" + probe)
+		//promises.push(tagQuery);		//TODO: This isn't right. Must find posts that contain these tags
+		promises.push(queryPostField("title"));
+		promises.push(queryPostField("notes"));
+	}
+	}
+	if (filter == Scope.All || filter == Scope.Albums || filter == Scope.Groups) {
+		// search groups and albums
+		function queryAlbumGroupField(type, field) {
+			var query = getQuery("Album");
+			query.equalTo("type", type);
+			if (type == "album")
+				query.equalTo("createdBy", user);
+			query.matches(field,  "(?i)" + probe);
+			return query.find()
+		}
+		if (filter != Scope.Groups) {
+			promises.push(queryAlbumGroupField("album", "title"));
+			promises.push(queryAlbumGroupField("album", "notes"));
+		}
+		if (filter != Scope.Albums) {
+			promises.push(queryAlbumGroupField("group", "title"));
+			promises.push(queryAlbumGroupField("group", "notes"));
+		}
+	}
+	if (filter == Scope.All || filter == Scope.Users) {
+		// search users
+		function queryUserField(field) {
+			var query = getQuery("_User");
+			query.matches(field,  "(?i)" + probe);
+			return query.find();
+		}
+		promises.push(queryUserField("username"));
+		promises.push(queryUserField("email"));
+		promises.push(queryUserField("name"));
+	}
+	Parse.Promise.when(promises).then(function(results) {
+		// TODO: sort the results
+		var finalResults = [];
+		_.each(results, function accum(r) { finalResults = finalResults.concat(r) });
+		finalResults = _.uniqBy(finalResults, function(a) { return a.id });
+		_.sort(finalResults, function(a, b) { return a.get("createdAt") > b.get("createdAt") });
+		response.success(finalResults);
+	});
+});
