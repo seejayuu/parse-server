f67590a6559c4ae373a409fa7c4b5b5cc76012a5
Chris Williams <cwilliams@alum.mit.edu>
2016-04-16 08:29:54 -0400

    Clarifai now uses node request package

--- cloud/recognizer/clarifai/main.js
+++ cloud/recognizer/clarifai/main.js
@@ -51,6 +51,7 @@ function tagURL(imageURL, imageID, completion) {
 			},
 			function(error, response, body) {
 				// upload the image and read back the tags
+				console.log(body);
 				request("https://" + apiURL + tagPath + '?access_token=' + body.access_token + '&url=' + imageURL,
 					function(error, response, body) {
 						if (error)
