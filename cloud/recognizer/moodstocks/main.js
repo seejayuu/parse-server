2975fc86c992a41492d4e1429dfb8e0017f646b8
Chris Williams <cwilliams@alum.mit.edu>
2016-04-16 08:59:34 -0400

    Clarifai now uses node request package

--- cloud/recognizer/moodstocks/main.js
+++ cloud/recognizer/moodstocks/main.js
@@ -2,12 +2,12 @@
 
 var api_key = "rhyn4dqidjhuoebkaxzu"
 var api_secret = "CN9RoCqSn92TAkLg"
-var apiURL = "api.moodstocks.com/v2"
+var apiURL = "api.moodstocks.com"
 
 //curl --digest -u YourApiKey:YourApiSecret "http://api.moodstocks.com/v2/search" --form image_url="http://www.example.com/bar.jpg"
 
 
-var searchPath = "/search";
+var searchPath = "/v2/search";
 
 function getTags(imageURL, imageID, completion) {
 	var results = []
@@ -24,14 +24,15 @@ function getTags(imageURL, imageID, completion) {
 	});
 }
 
-var digest = require('../../util/http-digest-client')(api_key, api_secret);
+var digest = require('http-digest-client').createDigestClient(api_key, api_secret);
 
 function tagURL(imageURL, completion) {
 	var obj = { image_url: imageURL };
   digest.request({
-    url: "http://" + apiURL + searchPath,
+    host: apiURL,
+    path: searchPath,
+    port: 80,
     method: 'POST',
-    digesturi: "/v2/search",
     body: Object.keys(obj).reduce(function(a,k){a.push(k+'='+encodeURIComponent(obj[k]));return a},[]).join('&'),
     success: function(response) {
       console.log("Moodstocks API success: " + JSON.stringify(response.data));
