6056ed662ac2b2e709f9b6cb88a38470bacb1a68
Chris Williams <cwilliams@alum.mit.edu>
2016-04-17 08:51:14 -0400

    Back end check for Pingdom

--- cloud/app.js
+++ cloud/app.js
@@ -34,7 +34,7 @@ app.use(express.static('public'));
 //////////////////////////////////////
 
 app.get('/backendcheck', function(request, response) {
-	request({
+	request.get({
 		url: "http://poppo.herokuapp.com/parse",
 		//headers: {
 		//	"X-Parse-Application-Id": 'r0KegEx2R4IO1Bk8ajoS'
