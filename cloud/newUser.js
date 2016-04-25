f62160213669c462072fe0ccc754fc49044e4f85
Chris Williams <cwilliams@alum.mit.edu>
2016-04-25 13:29:50 -0400

    Reverse geocoding

--- cloud/newUser.js
+++ cloud/newUser.js
@@ -7,7 +7,7 @@ Parse.Cloud.define("newUser", function(request, response) {
 	var thisUser = request.user;
 	var locations = [];
 	console.log("*****newUser photo count=" + request.params.roll.length);
-	console.log("******first photo: " + JSON.stringify(request.params.roll[0]);
+	console.log("******first photo: " + JSON.stringify(request.params.roll[0]));
 	// find all the locations in the camera roll, count the photos at each location, find length of time at location
 	// { id: unique_id, location: location_string, date: time_taken }
 	var albums = _.groupBy(request.params.roll, function(a) { return a.location }).sortBy(function(b) { -b.length });
