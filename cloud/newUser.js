e9150cbd418ff2f24fc3ce46fae8737485f7d99b
Chris Williams <cwilliams@alum.mit.edu>
2016-04-25 19:10:36 -0400

    Reverse geocoding

--- cloud/newUser.js
+++ cloud/newUser.js
@@ -57,7 +57,7 @@ Parse.Cloud.define("newUser", function(request, response) {
 								post.set("postedAt", albumPost.date);
 								post.set("persistentID", albumPost.id);
 								post.setACL(worldACL);
-								var point = new Parse.GeoPoint(latitude: albumPost.location.latitude, longitude: albumPost.location.longitude);
+								var point = new Parse.GeoPoint({latitude: albumPost.location.latitude, longitude: albumPost.location.longitude});
 								post.set("location", point);
 								var relation = post.relation("albums");
 								relation.add(album);
