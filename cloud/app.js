df4badec62d62bc99d0f461ff81ac3696616b0c4
seejayuu <cwilliams@alum.mit.edu>
2016-05-15 08:14:27 -0700

    IR photo count limit lowered; findPoppoUsers returns whole user object

--- cloud/app.js
+++ cloud/app.js
@@ -297,10 +297,8 @@ function scan(className, userFieldName, deleteFlag, callback) {
         user.getObject(className, result.id, function(obj) {
           obj.destroy({
             success: function() {
-              console.log("***deleted: " + result.id);
             },
             error: function() {
-              console.log("***error***");
             }
           });
         });
