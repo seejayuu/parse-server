6f257b235298c101d5c94945fb0dfd583063952d
Chris Williams <cwilliams@alum.mit.edu>
2016-05-25 06:58:26 -0400

    Uses gzip compression for all requests

---
+++ curl.sh
@@ -0,0 +1,6 @@
+curl -v -X GET \
+  -H "Accept-Encoding: gzip" \
+  -H "X-Parse-Application-Id: r0KegEx2R4IO1Bk8ajoS" \
+  -H "X-Parse-Master-Key: yQpIBp1jYc0yRVpUZYYU" \
+  http://localhost:1337/parse/$1
+  
